879fa2fbf3e1c2fe4caf5941a3f6bfeea018959a
Aukiha <aukihat@gmail.com>
2018-01-10 17:54:18 -0500

    Added some more functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import jdk.nashorn.internal.scripts.JO;
+
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.*;
 
@@ -26,6 +33,7 @@ public class Main {
         // JPanel that will contain the main text editor field
         JPanel frame = new JPanel();
         frame.setLayout(new FlowLayout());
+        frame.setBorder(new TitledBorder(new EtchedBorder(), "Entry Field"));
 
         // JPanel that will contain the file input field
         JPanel frame2 = new JPanel();
@@ -35,10 +43,17 @@ public class Main {
         JPanel frame3 = new JPanel();
         frame3.setLayout(new FlowLayout());
 
+        JPanel frame4 = new JPanel();
+        frame4.setLayout(new FlowLayout());
+
 
         // Frame Panel Elements - Text Field for user input
         JTextArea textInput = new JTextArea(30, 80);
-        frame.add(textInput);
+        textInput.setWrapStyleWord(true);
+        textInput.setLineWrap(true);
+        JScrollPane scroll = new JScrollPane(textInput);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        frame.add(scroll);
 
         // Frame2 Panel Elements - Text Field for file name input
         JTextField fileNameInput = new JTextField(20);
@@ -56,16 +71,84 @@ public class Main {
         frame3.add(saveButton);
         frame3.add(loadButton);
 
+        // Frame4 Panel Elements
+        JButton updateDirButton = new JButton("Update List");
+        JLabel filesInDirectoryLabel = new JLabel("Files in current directory: ");
+        JTextArea filesList = new JTextArea(10, 10);
+        frame4.add(filesInDirectoryLabel);
+        frame4.add(updateDirButton);
+        frame4.add(filesList);
+
+        // checks if the file name input field is empty
+        fileNameInput.getDocument().addDocumentListener(new DocumentListener() {
+            public void insertUpdate(DocumentEvent documentEvent) {
+                if(fileNameInput.getText().equals("")) {
+                    saveButton.setEnabled(false);
+                }
+                else {
+                    saveButton.setEnabled(true);
+                }
+            }
+
+            public void removeUpdate(DocumentEvent documentEvent) {
+                if(fileNameInput.getText().equals("")) {
+                    saveButton.setEnabled(false);
+                }
+                else {
+                    saveButton.setEnabled(true);
+                }
+
+            }
+
+            public void changedUpdate(DocumentEvent documentEvent) {
+                if(fileNameInput.getText().equals("")) {
+                    saveButton.setEnabled(false);
+                }
+                else {
+                    saveButton.setEnabled(true);
+                }
+            }
+        });
+
+        // checks if the format input field is empty
+        formatInput.getDocument().addDocumentListener(new DocumentListener() {
+            public void insertUpdate(DocumentEvent documentEvent) {
+                if(formatInput.getText().equals("")) {
+                    saveButton.setEnabled(false);
+                }
+                else {
+                    saveButton.setEnabled(true);
+                }
+            }
+
+            public void removeUpdate(DocumentEvent documentEvent) {
+                if(formatInput.getText().equals("")) {
+                    saveButton.setEnabled(false);
+                }
+                else {
+                    saveButton.setEnabled(true);
+                }
+
+            }
+
+            public void changedUpdate(DocumentEvent documentEvent) {
+                if(formatInput.getText().equals("")) {
+                    saveButton.setEnabled(false);
+                }
+                else {
+                    saveButton.setEnabled(true);
+                }
+            }
+        });
+
         // Button action listener that will trigger if the user clicks on saveButton
         saveButton.addActionListener(new ActionListener() {
-            //String format = ".docx"; // default file format
-            //String format = "."+(formatInput.getText());
             public void actionPerformed(ActionEvent actionEvent) {
             File doc = null;
             FileWriter out = null;
 
             try {
-                doc = new File(fileNameInput.getText()+"."+formatInput.getText()); // creates a new document with the name given by the user and the format
+                doc = new File(fileNameInput.getText() + "." + formatInput.getText()); // creates a new document with the name given by the user and the format
                 out = new FileWriter(doc);
                 out.write(textInput.getText()); // writes the text in the main editor to the file
                 out.close(); // closes the file writer
@@ -80,7 +163,24 @@ public class Main {
 
             }
 
+
+
         }); // End of function
+
+        updateDirButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent actionEvent) {
+                filesList.setText("");
+                File list = new File(".");
+                File[] listA = list.listFiles();
+                for(int i = 0; i < listA.length-1; i++) {
+                    File temp = listA[i];
+                    if(temp.isFile() && (temp.getName().endsWith(".docx") || temp.getName().endsWith(".txt") || temp.getName().endsWith(".md"))) {
+                        filesList.append(temp + "\n");
+                    }
+                }
+            }
+        });
 
 
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -92,6 +192,7 @@ public class Main {
 
         optionsFrame.add(frame2);
         optionsFrame.add(frame3);
+        optionsFrame.add(frame4);
         optionsFrame.pack();
         optionsFrame.setVisible(true);
     }
